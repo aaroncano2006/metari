2f5a9c1fb864bcc2e7097fab9d2e3b0490a71e96
Adria <adria_borrascarrillo@iescarlesvallbona.cat>
2026-04-16 01:29:09 +0200

    Afegit bootstrap

--- metari-app/src/App.tsx
+++ metari-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-warning'>Benvingut a Metari</h1>
+      <h1 className='bg-warning text-center'>Benvingut a Metari</h1>
     </>
   )
 }
