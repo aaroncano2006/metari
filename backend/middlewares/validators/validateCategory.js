c24d8d2b4c11b901f70e701c806816fa91e19370
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-04-15 17:09:51 +0200

    Validador de categoria creat.

---
+++ backend/middlewares/validators/validateCategory.js
@@ -0,0 +1,24 @@
+const validateCategory = (data) => {
+
+    if (!data.name) {
+        return "El nom de la categoria és obligatori!";
+    }
+
+    if (typeof(data.name) !== "string") {
+        return "El nom enviat no és vàlid! Ha de ser un text";
+    }
+
+    if (data.name.trim() = "") {
+        return "El nom enviat està buit.";
+    }
+
+    if (data.description && typeof(data.description) !== "string") {
+        return "La descripció enviada no és vàlida! Ha de ser un text";
+    }
+
+    return true;
+};
+
+module.exports = {
+    validateCategory
+};
