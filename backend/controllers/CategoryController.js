a5ab085fd6785aab0223abcbd1024e0975bc8ceb
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-04-15 17:00:12 +0200

    Ús d'errorHandler al controlador de categories per a getCategoryById, createCategory i updateCategory.

--- backend/controllers/CategoryController.js
+++ backend/controllers/CategoryController.js
@@ -1,16 +1,17 @@
 const prisma = require("../config/prisma");
 
-const getCategories = async (req, res) => {
+const getCategories = async (req, res, next) => {
   try {
     const categories = await prisma.category.findMany();
     res.status(200).json(categories);
   } catch (error) {
     console.error("Error en Prisma:", error);
-    res.status(500).json({ error: "Error carregant categories" });
+    // res.status(500).json({ error: "Error carregant categories" });
+    next(error);
   }
 };
 
-const getCategoryById = async (req, res) => {
+const getCategoryById = async (req, res, next) => {
   try {
     const id = parseInt(req.params.id);
 
@@ -18,16 +19,31 @@ const getCategoryById = async (req, res) => {
       where: { id },
     });
 
+    // Aquesta condició si es manté
+    if (!category) {
+      const error = new Error("No s'ha trobat la categoria");
+      error.statusCode = 404;
+      throw error;
+    }
+
     res.status(200).json(category);
   } catch (error) {
     console.error("Error en Prisma:", error);
-    res.status(500).json({ error: "Error carregant categoria" });
+    // res.status(500).json({ error: "Error carregant categoria" });
+    next(error);
   }
 };
 
-const createCategory = async (req, res) => {
+const createCategory = async (req, res, next) => {
   try {
     const category = req.body;
+
+    // Aquesta condició serà modificada quan afegim els validadors.
+    if (!category.name || typeof(category.name) !== "string") {
+      const error = new Error("Invalid category name");
+      error.statusCode = 404;
+      throw error;
+    }
 
     const newCategory = await prisma.category.create({
       data: {
@@ -42,15 +58,34 @@ const createCategory = async (req, res) => {
     res.status(201).json(newCategory);
   } catch (error) {
     console.error("Error en Prisma:", error);
-    res.status(500).json({ error: "Error creant categoria" });
+    // res.status(500).json({ error: "Error creant categoria" });
+    next(error);
   }
 };
 
-const updateCategory = async (req, res) => {
+const updateCategory = async (req, res, next) => {
   try {
     const id = parseInt(req.params.id);
 
+    // const category = await prisma.category.findUnique({
+    //   where: { id },
+    // });
+
+    // // Aquesta condició si es manté
+    // if (!category) {
+    //   const error = new Error("No s'ha trobat la categoria");
+    //   error.statusCode = 404;
+    //   throw error;
+    // }
+
     const data = req.body;
+
+    // Aquesta condició serà modificada quan afegim els validadors.
+    if (!data.name || typeof(data.name) !== "string") {
+      const error = new Error("Invalid category name");
+      error.statusCode = 400;
+      throw error;
+    }
 
     const updatedData = await prisma.category.update({
       where: { id },
@@ -64,11 +99,12 @@ const updateCategory = async (req, res) => {
     res.status(200).json(updatedData);
   } catch (error) {
     console.error("Error en Prisma:", error);
-    res.status(500).json({ error: "Error actualitzant categoria" });
+    // res.status(500).json({ error: "Error actualitzant categoria" });
+    next(error);
   }
 };
 
-const deleteCategory = async (req, res) => {
+const deleteCategory = async (req, res, next) => {
   try {
     const id = parseInt(req.params.id);
 
@@ -83,7 +119,8 @@ const deleteCategory = async (req, res) => {
     });
   } catch (error) {
     console.error("Error en Prisma:", error);
-    res.status(500).json({ error: "Error eliminant categoria" });
+    // res.status(500).json({ error: "Error eliminant categoria" });
+    next(error);
   }
 };
 
