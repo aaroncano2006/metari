f8109b3c908e7417314bb37780f0b404f6d6363c
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-04-20 19:59:12 +0200

    Refactor de validateInvitation per millorar l'eficiencia de les consultes.

--- backend/middlewares/validators/validateInvitation.js
+++ backend/middlewares/validators/validateInvitation.js
@@ -1,43 +1,22 @@
 const prisma = require("../../config/prisma");
 
 const validateInvitation = async (data) => {
-  if (isNaN(data.sender_id)) {
-    return "La id del emissor no és vàlida!";
-  }
+  if (isNaN(data.sender_id) || isNaN(data.receiver_id)) return "IDs no vàlids";
+  if (data.sender_id === data.receiver_id)
+    return "No et pots convidar a tu mateix";
 
-  const sender = await prisma.user.findUnique({
-    where: { id: data.sender_id },
-  });
+  const [sender, receiver, group] = await Promise.all([
+    prisma.user.findUnique({ where: { id: data.sender_id } }),
+    prisma.user.findUnique({ where: { id: data.receiver_id } }),
+    data.group_id
+      ? prisma.group.findUnique({ where: { id: data.group_id } })
+      : Promise.resolve(null),
+  ]);
 
-  if (isNaN(data.receiver_id)) {
-    return "La id del receptor no és vàlida!";
-  }
+  if (!sender || !receiver) return "L'emissor o el receptor no existeixen";
+  if (data.group_id && !group) return "El grup no existeix";
 
-  const receiver = await prisma.user.findUnique({
-    where: { id: data.receiver_id },
-  });
-
-  if (data.group_id && isNaN(data.groupId)) {
-    return "La id del grup no és vàlida!";
-  }
-
-  const group = data.group_id
-    ? await prisma.group.findUnique({
-        where: { id: data.group_id },
-      })
-    : null;
-
-  if (sender === receiver) {
-    return "No et pots enviar sol·licitud d'amistat a tu mateix!";
-  }
-
-  if (!sender || !receiver) {
-    return "No s'ha trobat emissor o receptor!";
-  }
-
-  return null;
+  return { sender, receiver, group };
 };
 
-module.exports = {
-  validateInvitation,
-};
+module.exports = { validateInvitation };
