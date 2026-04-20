b3dcd05fd8a82a20976281aad64a646615bb4093
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-04-20 18:55:58 +0200

    Validació bàsica de metes creada (s'ha de testejar i implementar)

--- backend/middlewares/validators/validateMeta.js
+++ backend/middlewares/validators/validateMeta.js
@@ -1,6 +1,48 @@
 const prisma = require("../../config/prisma");
 
-const validateMeta = async (data, id = null) => {};
+const validateMeta = async (data) => {
+    if (!data.title) {
+        return "El títol de la meta és obligatori";
+    }
+
+    if (typeof data.title !== "string") {
+        return "El títol enviat no és vàlid! Ha de ser un text.";
+    }
+
+    if (data.title.trim() === "") {
+        return "El títol enviat està buit.";
+    }
+
+    if (data.description && typeof data.description !== "string") {
+        return "La descripció enviada no és vàlida! Ha de ser un text.";
+    }
+
+    const existingAuthor = await prisma.user.findUnique({
+        where: {id: parseInt(data.author_id)}
+    });
+
+    if (!existingAuthor) {
+        return "La id de l'autor enviada no correspon a cap usuari registrat!";
+    }
+
+    const existingGroup = await prisma.group.findUnique({
+        where: {id: parseInt(data.group_id)}
+    })
+
+    if (!existingGroup) {
+        return "La id del grup enviada no correspon a cap grup registrat en el sistema."
+    }
+
+    if (!data.type) {
+        return "El tipus de la meta és obligatori!";
+    }
+
+    if (data.type && data.type !== "task" && data.type !== "challenge") {
+        return "El tipus de meta no és vàlid!";
+    }
+
+    return null;
+};
 
 module.exports = {
   validateMeta,
