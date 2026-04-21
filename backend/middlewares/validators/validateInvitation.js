c4be46c584d863f3e09c50632d3fafd267a9260a
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-04-21 11:41:00 +0200

    Comptador d'invitacions per evitar sol·licituds que involucrin als mateixos usuaris i grups.

--- backend/middlewares/validators/validateInvitation.js
+++ backend/middlewares/validators/validateInvitation.js
@@ -5,6 +5,42 @@ const validateInvitation = async (data) => {
   if (data.sender_id === data.receiver_id)
     return "No et pots convidar a tu mateix";
 
+  const countInv = await prisma.invitation.count({
+    where: data.group_id
+      ? {
+          OR: [
+            {
+              sender_id: data.sender_id,
+              receiver_id: data.receiver_id,
+              group_id: data.group_id,
+            },
+            {
+              sender_id: data.receiver_id,
+              receiver_id: data.sender_id,
+              group_id: data.group_id,
+            },
+          ],
+        }
+      : {
+          OR: [
+            {
+              sender_id: data.sender_id,
+              receiver_id: data.receiver_id,
+              group_id: null,
+            },
+            {
+              sender_id: data.receiver_id,
+              receiver_id: data.sender_id,
+              group_id: null,
+            },
+          ],
+        },
+  });
+
+  if (countInv > 0) {
+    return "Ja existeix una sol·licitud amb aquestes característiques.";
+  }
+
   const [sender, receiver, group] = await Promise.all([
     prisma.user.findUnique({ where: { id: data.sender_id } }),
     prisma.user.findUnique({ where: { id: data.receiver_id } }),
