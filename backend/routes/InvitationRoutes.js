5c914a0fbb62e174f7697cd91a72476079d7a84b
Aaron Cano <aaroncanofdez@gmail.com>
2026-04-22 12:06:16 +0200

    Ja pots consultar quines invitacions ha enviat o rebut l'usuari amb tots els estats possibles.

--- backend/routes/InvitationRoutes.js
+++ backend/routes/InvitationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const invitationController = require('../controllers/InvitationController');
 
-router.get('/:userid/:status', invitationController.getInvitations);
+router.get('/:userid/:sentorreceived/:status', invitationController.getInvitations); // segon param "sent" o "received"
 router.post('/:senderid/:receiverid', invitationController.sendInvitations);
 router.post('/:senderid/:receiverid/:groupid', invitationController.sendInvitations);
 router.put('/:receiverid/:id', invitationController.acceptInvitation);
