d6e125742cdc9e85634c534353e3d6a641a72866
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-04-24 15:55:02 +0200

    Control d'ID invàlida a AssingationController i include de totes les entitats involucrades en l'assignació al fer GET.

--- backend/controllers/AssignationController.js
+++ backend/controllers/AssignationController.js
@@ -4,7 +4,15 @@ const utils = require("../helpers/Utils");
 // GET ALL
 const getAssignations = async (req, res, next) => {
   try {
-    const assignations = await prisma.assignation.findMany();
+    const assignations = await prisma.assignation.findMany({
+      include: {
+        group: true,
+        meta: true,
+        user: true,
+        comments: true,
+        proofs: true
+      }
+    });
     res.status(200).json(utils.handleBigInt(assignations));
   } catch (error) {
     console.error("Error en Prisma:", error);
@@ -15,87 +23,97 @@ const getAssignations = async (req, res, next) => {
 
 const getAssignationById = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+      const error = new Error("ID invàlida!");
+      error.statusCode = 400;
+      throw error;
+    }
 
     const assignation = await prisma.assignation.findUnique({
-      where: { id: Number(id) },
-      // include: {
-      //   group: true,
-      //   meta: true,
-      //   user: true,
-      //   comments: true,
-      //   proofs: true,
-      // }
+      where: { id },
+      include: {
+        group: true,
+        meta: true,
+        user: true,
+        comments: true,
+        proofs: true,
+      },
     });
 
     res.status(200).json(utils.handleBigInt(assignation));
   } catch (error) {
     console.error("Error en Prisma:", error);
     next(error);
-
   }
 };
 
 const createAssignation = async (req, res, next) => {
-    const reqBody = req.body;
+  const reqBody = req.body;
 
-    // si els 2 estan definits o ningun esta definit, error
-    if ((reqBody.group_id && reqBody.user_id) || (!reqBody.group_id && !reqBody.user_id)) {
-        return res.status(400).json({
-            error: "You must provide either group_id or user_id, but not both or neither.",
-        });
-    }
+  // si els 2 estan definits o ningun esta definit, error
+  if (
+    (reqBody.group_id && reqBody.user_id) ||
+    (!reqBody.group_id && !reqBody.user_id)
+  ) {
+    return res.status(400).json({
+      error:
+        "You must provide either group_id or user_id, but not both or neither.",
+    });
+  }
 
-    try {
-        const assignation = await prisma.assignation.create({
-            data: {
-                group_id: reqBody.group_id ? parseInt(reqBody.group_id) : null,
-                meta_id: parseInt(reqBody.meta_id),
-                user_id: reqBody.user_id ? parseInt(reqBody.user_id) : null,
-                start_date: reqBody.start_date,
-                due_date: reqBody.due_date,
-                priority: reqBody.priority,
-                difficulty: reqBody.difficulty ?? "normal",
-                score: reqBody.score ? BigInt(reqBody.score) : null,
-                completed: reqBody.completed ?? false,
-            },
-        });
+  try {
+    const assignation = await prisma.assignation.create({
+      data: {
+        group_id: reqBody.group_id ? parseInt(reqBody.group_id) : null,
+        meta_id: parseInt(reqBody.meta_id),
+        user_id: reqBody.user_id ? parseInt(reqBody.user_id) : null,
+        start_date: reqBody.start_date,
+        due_date: reqBody.due_date,
+        priority: reqBody.priority,
+        difficulty: reqBody.difficulty ?? "normal",
+        score: reqBody.score ? BigInt(reqBody.score) : null,
+        completed: reqBody.completed ?? false,
+      },
+    });
 
-        res.status(201).json(utils.handleBigInt(assignation));
-    } catch (error) {
-        console.error("Error en Prisma:", error);
-        // res.status(500).json({ error: "Error al crear assignació" });
-        next(error);
-
-    }
+    res.status(201).json(utils.handleBigInt(assignation));
+  } catch (error) {
+    console.error("Error en Prisma:", error);
+    // res.status(500).json({ error: "Error al crear assignació" });
+    next(error);
+  }
 };
-
 
 const updateAssignation = async (req, res, next) => {
   const reqBody = req.body;
   const { id } = req.params;
 
-  if ((reqBody.group_id && reqBody.user_id) || (!reqBody.group_id && !reqBody.user_id)) {
-        return res.status(400).json({
-            error: "You must provide either group_id or user_id, but not both or neither.",
-        });
-    }
+  if (
+    (reqBody.group_id && reqBody.user_id) ||
+    (!reqBody.group_id && !reqBody.user_id)
+  ) {
+    return res.status(400).json({
+      error:
+        "You must provide either group_id or user_id, but not both or neither.",
+    });
+  }
 
   try {
-
     const updatedAssignation = await prisma.assignation.update({
       where: { id: Number(id) },
       data: {
-                group_id: reqBody.group_id ? parseInt(reqBody.group_id) : null,
-                meta_id: parseInt(reqBody.meta_id),
-                user_id: reqBody.user_id ? parseInt(reqBody.user_id) : null,
-                start_date: reqBody.start_date,
-                due_date: reqBody.due_date,
-                priority: reqBody.priority,
-                difficulty: reqBody.difficulty ?? "normal",
-                score: reqBody.score ? BigInt(reqBody.score) : null,
-                completed: reqBody.completed ?? false,
-            },
+        group_id: reqBody.group_id ? parseInt(reqBody.group_id) : null,
+        meta_id: parseInt(reqBody.meta_id),
+        user_id: reqBody.user_id ? parseInt(reqBody.user_id) : null,
+        start_date: reqBody.start_date,
+        due_date: reqBody.due_date,
+        priority: reqBody.priority,
+        difficulty: reqBody.difficulty ?? "normal",
+        score: reqBody.score ? BigInt(reqBody.score) : null,
+        completed: reqBody.completed ?? false,
+      },
     });
 
     res.status(200).json(utils.handleBigInt(updatedAssignation));
@@ -103,7 +121,6 @@ const updateAssignation = async (req, res, next) => {
     console.error("Error en Prisma:", error);
     // res.status(500).json({ error: "Error al actualitzar assignació" });
     next(error);
-
   }
 };
 
@@ -119,7 +136,6 @@ const deleteAssignation = async (req, res, next) => {
   } catch (error) {
     console.error("Error en Prisma:", error);
     next(error);
-
   }
 };
 
@@ -130,5 +146,3 @@ module.exports = {
   updateAssignation,
   deleteAssignation,
 };
-
-
