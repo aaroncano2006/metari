2b0adf56637eb07c634bf5ff66cf800eddfe6a66
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-03 19:01:44 +0200

    Funció per obtenir el rol de l'usuari mitjançant el token desat a localStorage

--- metari-app/src/services/auth/loginService.ts
+++ metari-app/src/services/auth/loginService.ts
@@ -5,3 +5,11 @@ export async function fetchLogin(data: loginType): Promise<any> {
   const response = await axiosConnection.post("/login", data);
   return response.data;
 }
+
+export function getUserRole(): string | null {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  return payload.role;
+}
