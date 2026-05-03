35ff1217b51e6222f1aba547e1570211fc2904ce
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-03 19:03:36 +0200

    Redirecció de /admin comprovant el rol de l'usuari autenticat via token.

--- metari-app/src/views/AdminPanel.tsx
+++ metari-app/src/views/AdminPanel.tsx
@@ -1,55 +1,81 @@
-import { MetaList } from "../components/MetaList"
-import { useEffect, useState } from "react"
-import { useMetas } from "../services/metaService"
-import { fetchCategories } from "../services/categoryService"
+import { MetaList } from "../components/MetaList";
+import { useEffect, useState } from "react";
+import { useMetas } from "../services/metaService";
+import { fetchCategories } from "../services/categoryService";
 import { CategoryList } from "../components/CategoryList";
+import { useNavigate } from "react-router-dom";
+import { getUserRole } from "../services/auth/loginService";
 
-import type { categoryType } from "../types/categoryType"
-
+import type { categoryType } from "../types/categoryType";
 
 export default function AdminPanel() {
+  const [menuSelection, setMenuSelection] = useState<string>("metas");
+  const [categories, setCategories] = useState<categoryType[]>([]);
 
-  const [menuSelection, setMenuSelection] = useState<string>("metas")
-  const [categories, setCategories] = useState<categoryType[]>([])
+  const navigate = useNavigate();
 
-  useEffect(() => {    
-    fetchCategories().then(setCategories)
+  useEffect(() => {
+    fetchCategories().then(setCategories);
+  }, []);
 
-  }, [])
+  useEffect(() => {
+    const token = localStorage.getItem("token");
 
+    if (!token) {
+      navigate("/login");
+    }
+
+    const userRole = getUserRole();
+
+    if (!userRole && userRole !== "admin") {
+      navigate("/");
+    }
+  }, []);
 
   //need to refactor again
   const metas = useMetas();
-
 
   return (
     <>
       <h1>Panell Admin</h1>
       <div className="selectionMenu mt-5 d-flex justify-content-center gap-3">
-        <div className="btn btn-primary"
-          onClick={() => setMenuSelection("metas")}>Metas</div>
-        <div className="btn btn-primary"
-          onClick={() => setMenuSelection("categories")}>Categories</div>
-        <div className="btn btn-primary"
-          onClick={() => setMenuSelection("usuaris")}>Usuaris</div>
-        <div className="btn btn-primary"
-          onClick={() => setMenuSelection("grups")}>Grups</div>
+        <div
+          className="btn btn-primary"
+          onClick={() => setMenuSelection("metas")}
+        >
+          Metas
+        </div>
+        <div
+          className="btn btn-primary"
+          onClick={() => setMenuSelection("categories")}
+        >
+          Categories
+        </div>
+        <div
+          className="btn btn-primary"
+          onClick={() => setMenuSelection("usuaris")}
+        >
+          Usuaris
+        </div>
+        <div
+          className="btn btn-primary"
+          onClick={() => setMenuSelection("grups")}
+        >
+          Grups
+        </div>
       </div>
       <div className="container-fluid">
         <div className="row mt-4">
-
-          <div className="col-3">
-
-          </div>
+          <div className="col-3"></div>
           <div className="col-6">
             {menuSelection === "metas" && <MetaList metas={metas} />}
-            {menuSelection === "categories" && <CategoryList categories={categories} setter={setCategories}/>}
+            {menuSelection === "categories" && (
+              <CategoryList categories={categories} setter={setCategories} />
+            )}
           </div>
-          <div className="col-3">
-          </div>
+          <div className="col-3"></div>
         </div>
       </div>
-
     </>
-  )
+  );
 }
