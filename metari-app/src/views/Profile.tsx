6081f197a529f7d0c93686b168c195786e779723
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-03 13:56:43 +0200

    Redirecció a /login si no existeix el token a profile.

--- metari-app/src/views/Profile.tsx
+++ metari-app/src/views/Profile.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function Profile() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      navigate("/login");
+    }
+  }, []);
+
   return <h1>Profile Page</h1>
 }
