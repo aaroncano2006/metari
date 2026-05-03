b87831edce5406999c9008881bee33c63e219181
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-03 13:54:05 +0200

    Renderitzat condicional a Navbar, si existeix el token pot mostrar l'enllaç al perfil, si no mostra els enllaços per a login o register.

--- metari-app/src/components/NavBar.tsx
+++ metari-app/src/components/NavBar.tsx
@@ -1,26 +1,33 @@
 import { Link } from "react-router-dom";
 
 export function NavBar() {
+  const token = localStorage.getItem("token");
 
-    return (
+  return (
     <>
-     <div className="navBar">
-      <Link to="/" className="nav-btn">
-        Home
-      </Link>
-      <Link to="/Profile" className="nav-btn">
-        Perfil
-      </Link>
-      <Link to="/Login" className="nav-btn">
-        Login
-      </Link>
-      <Link to="/Register" className="nav-btn">
-        Registra't
-      </Link>
-      <Link to="/Admin" className="nav-btn">
-        Panell Admin
-      </Link>
-     </div>
+      <div className="navBar">
+        <Link to="/" className="nav-btn">
+          Home
+        </Link>
+        {token && (
+          <Link to="/Profile" className="nav-btn">
+            Perfil
+          </Link>
+        )}
+        {!token && (
+          <Link to="/Login" className="nav-btn">
+            Login
+          </Link>
+        )}
+        {!token && (
+          <Link to="/Register" className="nav-btn">
+            Registra't
+          </Link>
+        )}
+        <Link to="/Admin" className="nav-btn">
+          Panell Admin
+        </Link>
+      </div>
     </>
   );
 }
