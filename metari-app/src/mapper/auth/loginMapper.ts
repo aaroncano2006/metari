0fcbe2a9c2af8cc0b6d69b6569ad54616e93b91e
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-04 16:32:35 +0200

    loginMapper per a iniciar sessió després de registrar-te.

---
+++ metari-app/src/mapper/auth/loginMapper.ts
@@ -0,0 +1,10 @@
+import type { loginType } from "../../types/auth/loginType"
+import type { registerType } from "../../types/auth/registerType"
+
+export function mapUser(register: registerType): loginType {
+  return {
+    email_or_username: register.email,
+    password: register.password,
+    remember_password: false,
+  }
+}
