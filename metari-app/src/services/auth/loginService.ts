ad75a1fb0d51fddb499642186ba518b5a918882d
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-04 18:46:59 +0200

    Nova funció per extreure el nom complert de l'usuari.

--- metari-app/src/services/auth/loginService.ts
+++ metari-app/src/services/auth/loginService.ts
@@ -21,3 +21,11 @@ export function getUserName() : string | null {
   const payload = JSON.parse(atob(token.split(".")[1]));
   return payload.username;
 }
+
+export function getUserFullName() : string | null {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  return payload.name;
+}
