b6a41c8d22f86c6635ad54e729c74a10871efd78
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-04 19:16:51 +0200

    funció getUserId a loginService

--- metari-app/src/services/auth/loginService.ts
+++ metari-app/src/services/auth/loginService.ts
@@ -29,3 +29,11 @@ export function getUserFullName() : string | null {
   const payload = JSON.parse(atob(token.split(".")[1]));
   return payload.name;
 }
+
+export function getUserId(): number | null {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  return payload.id;
+}
