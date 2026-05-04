de94ae141b6f5118785470b80ce9a8692556239d
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-04 16:33:17 +0200

    Renamed function

--- metari-app/src/mapper/auth/loginMapper.ts
+++ metari-app/src/mapper/auth/loginMapper.ts
@@ -1,7 +1,7 @@
 import type { loginType } from "../../types/auth/loginType"
 import type { registerType } from "../../types/auth/registerType"
 
-export function mapUser(register: registerType): loginType {
+export function mapLogin(register: registerType): loginType {
   return {
     email_or_username: register.email,
     password: register.password,
