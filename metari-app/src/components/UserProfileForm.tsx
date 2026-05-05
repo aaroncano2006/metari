5ea350013b97bd6f2065b33231d2d0775bf3c1be
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-05 18:39:54 +0200

    UserProfileForm funcional.

--- metari-app/src/components/UserProfileForm.tsx
+++ metari-app/src/components/UserProfileForm.tsx
@@ -1,31 +1,140 @@
 import { useState } from "react";
-import { getUserEmail, getUserFullName, getUserName } from "../services/auth/loginService";
+import {
+  getUserEmail,
+  getUserFullName,
+  getUserId,
+  getUserName,
+} from "../services/auth/loginService";
 import type { profileType } from "../types/auth/profileType";
+import { updateProfile } from "../services/auth/profileService";
+import { emailExists, usernameExists } from "../services/userService";
 
 export default function UserProfileForm() {
   const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
 
   const rememberedPassword = localStorage.getItem("password");
 
   const fullName = getUserFullName() ?? "";
   const username = getUserName() ?? "";
   const email = getUserEmail() ?? "";
+  const userId = getUserId() ?? 0;
 
   const [formData, setFormData] = useState<profileType>({
     name: fullName,
     username,
     email,
-    password: rememberedPassword ?? ""
+    password: rememberedPassword ?? "",
   });
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (data: profileType) => {
+    setError(null);
+    setSuccess(false);
+
+    if (!data.name.trim()) {
+      return setError("El nom és obligatori!");
+    }
+
+    if (data.name && typeof data.name !== "string") {
+      return setError("El nom enviat no és vàlid! Ha de ser un text");
+    }
+
+    if (!data.username.trim()) {
+      return setError("El nom d'usuari és obligatori!");
+    }
+
+    if (data.username) {
+      if (typeof data.username !== "string") {
+        return setError(
+          "El nom d'usuari enviat no és vàlid! Ha de ser un text",
+        );
+      }
+
+      if (!/[a-zA-Z0-9]+$/.test(data.username)) {
+        return setError(
+          "El nom d'usuari només pot contenir lletres i números!",
+        );
+      }
+
+      if (data.username.length < 5) {
+        return setError("El nom d'usuari ha de contenir almenys 5 caràcters!");
+      }
+
+      const existingUsername = await usernameExists(data.username, userId);
+
+      if (existingUsername) {
+        return setError("El nom d'usuari introduït ja està registrat!");
+      }
+    }
+
+    if (!data.email.trim()) {
+      return setError("L'email és obligatori!");
+    }
+
+    if (data.email) {
+      if (typeof data.email !== "string") {
+        return setError("L'email de l'usuari no és vàlid! Ha de ser un text");
+      }
+
+      if (
+        !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._%+-]+\.[a-zA-Z]{2,}$/.test(data.email)
+      ) {
+        return setError("El format de l'email no és vàlid!");
+      }
+
+      const existingEmail = await emailExists(data.email, userId);
+
+      if (existingEmail) {
+        return setError("L'email introduït ja està registrat!");
+      }
+    }
+
+    if (!data.password?.trim()) {
+      return setError("La contrasenya és obligatòria");
+    }
+
+    if (data.password) {
+      if (typeof data.password !== "string") {
+        return setError(
+          "La contrasenya introduïda no és vàlida! Ha de ser un text!",
+        );
+      }
+
+      if (data.password.length < 8) {
+        return setError("La contrasenya ha de contenir almenys 8 caràcters!");
+      }
+    }
+
+    try {
+      const response = await updateProfile(data);
+
+      console.log(response);
+
+      if (response.token) {
+        localStorage.setItem("token", response.token);
+      }
+
+      setSuccess(true);
+    } catch (error: any) {
+      setError("Error: " + error);
+    }
+  };
 
   return (
     <>
+      {success && 
+      <div className="alert alert-success alert-profile">
+        Dades actualitzades correctament!
+      </div>}
+      {error && 
+      <div className="alert alert-danger alert-profile">
+        {error}
+      </div>
+      }
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          handleSubmit();
+          handleSubmit(formData);
         }}
       >
         <div className="row mb-2 user-profile-form-row">
