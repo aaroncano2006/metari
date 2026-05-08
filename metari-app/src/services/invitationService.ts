4668b7d522550fdaa75654078cd3a085f13152fb
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-08 17:29:11 +0200

    Servei per enviar invitacions (s'ha d'implementar i testejar)

--- metari-app/src/services/invitationService.ts
+++ metari-app/src/services/invitationService.ts
@@ -7,3 +7,13 @@ export async function fetchFriends(userId: number): Promise<userTypeFrontend[]> 
   const { data } = await axiosConnection.get<userTypeFrontend[]>(`/invitacions/friends/${userId}`)
   return data
 }
+
+export async function sendInvitation(senderId: number, receiverId: number, groupId: number | null): Promise<any> {
+  let BASE_URL = `/invitacions/${senderId}/${receiverId}`;
+  if (groupId) {
+    BASE_URL = BASE_URL + `/${groupId}`;
+  }
+
+  const { data } = await axiosConnection.post(BASE_URL);
+  return data;
+}
