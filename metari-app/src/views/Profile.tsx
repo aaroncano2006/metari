68129b795772e1ee0fff84149fa256eaac825e19
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-08 17:22:09 +0200

    Pàgina de perfil millorada, ara soporta els següents casos:
    - Sense search param: Redirigeix al teu perfil i et permet editar.
    
    - Amb search param username diferent al loguejat: Mostra el perfil de l'usuari a buscar sense el formulari d'edició.
    
    - Amb search param username igual al username de l'usuari loguejat: Redirigeix al teu perfil i et permet editar.
    
    - Amb search param username inexistent: Renderitza un error ja que no troba el username.

--- metari-app/src/views/Profile.tsx
+++ metari-app/src/views/Profile.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { UserProfilePicture } from "../components/UserProfilePicture";
 import {
   getUserFullName,
@@ -8,13 +8,48 @@ import {
 } from "../services/auth/loginService";
 import UserProfileForm from "../components/UserProfileForm";
 import UserProfileStats from "../components/UserProfileStats";
+import { getUserProfileData } from "../services/auth/profileService";
 
 export default function Profile() {
+  // Redireccions i recarrega dinàmica de la pàgina
   const navigate = useNavigate();
-  const name = getUserFullName();
-  const username = getUserName();
-  const stats = getUserStats();
   const [_recharge, setRecharge] = useState(0);
+
+  const [error, setError] = useState<string | null>(null);
+
+  // Controla si s'està consultant el perfil de l'usuari loguejat o d'un altre usuari.
+  const [searchParams] = useSearchParams();
+  const usernameSearchParam = searchParams.get("username") || "";
+  const [userData, setUserData] = useState<any>(null);
+  const name = userData?.name || getUserFullName();
+  const username = userData?.username || getUserName();
+  const stats = userData
+    ? {
+        score: userData.score,
+        completed_tasks: userData.completed_tasks,
+      }
+    : getUserStats();
+  useEffect(() => {
+    const loadProfile = async () => {
+      try {
+        const user = await getUserProfileData(usernameSearchParam);
+        if (user) {
+          if (user.username === username) {
+            return navigate("/profile");
+          }
+          setUserData(user);
+        } else {
+          throw new Error(`No s'ha trobat cap usuari amb el username: `);
+        }
+      } catch (err: any) {
+        setError(err.message);
+      }
+    };
+
+    if (usernameSearchParam) {
+      loadProfile();
+    }
+  }, []);
 
   useEffect(() => {
     const handleRecharge = () => setRecharge((cur) => cur + 1);
@@ -33,31 +68,40 @@ export default function Profile() {
   return (
     <>
       <div className="container-fluid p-3">
-        <header className="ms-5 p-5">
-          <h1>Profile</h1>
-        </header>
+        {!error && (
+          <>
+            <header className="ms-5 p-5">
+              <h1>Profile</h1>
+            </header>
 
-        <div className="row ms-5">
-          <div className="col-md-4">
-            <UserProfilePicture
-              url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwo8eJYb8h6_V7THlADVmoSbVkJQw6k08Liw&s"
-              id="userProfilePictureFromPage"
-            />
-            <div className="mt-3">
-              <h2>{name}</h2>
-              <h3>{username}</h3>
+            <div className="row ms-5">
+              <div className="col-md-4">
+                <UserProfilePicture
+                  url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwo8eJYb8h6_V7THlADVmoSbVkJQw6k08Liw&s"
+                  id="userProfilePictureFromPage"
+                />
+                <div className="mt-3">
+                  <h2>{name}</h2>
+                  <h3>{username}</h3>
+                </div>
+              </div>
+              <div className="col-md-5 mb-2">
+                {!userData && <UserProfileForm></UserProfileForm>}
+              </div>
+              <div className="col-md-2">
+                <UserProfileStats
+                  completed_tasks={stats?.completed_tasks}
+                  score={stats?.score}
+                ></UserProfileStats>
+              </div>
             </div>
-          </div>
-          <div className="col-md-5 mb-2">
-            <UserProfileForm></UserProfileForm>
-          </div>
-          <div className="col-md-2">
-            <UserProfileStats
-              completed_tasks={stats?.completed_tasks}
-              score={stats?.score}
-            ></UserProfileStats>
-          </div>
-        </div>
+          </>
+        )}
+        {error && (
+          <>
+            <div className="alert alert-danger">{error}<strong>{usernameSearchParam}</strong></div>
+          </>
+        )}
       </div>
     </>
   );
