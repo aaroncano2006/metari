3d57f3b89043a342d6452afbeced85492d289826
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-10 16:35:32 +0200

    Enllaç a perfil de l'usuari en el llistat d'amics

--- metari-app/src/components/FriendList.tsx
+++ metari-app/src/components/FriendList.tsx
@@ -46,7 +46,7 @@ export function FriendList({ users, setter }: UserListProps) {
                       <div className="d-flex py-1 ps-2 pe-2  align-items-center">
                         <div className="me-auto">{user.username}</div>
                         {token &&
-                          <Link to="/Profile" className="btn btn-primary p-1 ">
+                          <Link to={`/Profile?username=${user.username}`} className="btn btn-primary p-1 ">
                             <i className="bi bi-person-fill"></i>
                           </Link>
                         }
