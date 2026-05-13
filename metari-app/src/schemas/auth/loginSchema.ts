7d44552bd7d72e268a23d7300429d41792fc4fc9
Aaron Cano <aaroncanofdez@gmail.com>
2026-05-13 12:02:53 +0200

    Validació de email_or_username flexible per als dos casos i validació de password amb missatges d'error.

--- metari-app/src/schemas/auth/loginSchema.ts
+++ metari-app/src/schemas/auth/loginSchema.ts
@@ -1,6 +1,11 @@
 import {z} from "zod";
 
 export const loginSchema = z.object({
-    email_or_username: z.string(),
-    password: z.string().min(8)
+    email_or_username: z.string().refine((el) => {
+        if (el.includes("@")) {
+            return z.string().email().safeParse(el).success; // Utilitzem safeParse per validar sense llençar errors i reutilitzar validacions existents.
+        }
+        return z.string().min(5).safeParse(el).success;
+    }, "Introdueix un email vàlid o un nom d'usuari de com a mínim 5 caràcters."),
+    password: z.string().min(8, "La contrasenya ha de ser mínim de 8 caràcters")
 });
