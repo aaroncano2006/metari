a573fc0803af19ce9e0f29ec86ee2ba4ecbb6c75
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-14 17:56:14 +0200

    Component footer bàsic creat (No implementat encara)

---
+++ metari-app/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="bg-black mt-5 p-5 d-flex justify-content-center text-white">
+                Metari - Comunitats, objectius i connexions 
+            </footer>
+        </>
+    )
+}
