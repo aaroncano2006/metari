cf53f3395fb579c2f91693bf69e46a970d43f473
acanodev <aaroncanofdez@gmail.com>
2026-05-16 17:47:22 +0200

    searchController bàsic per trobar usuaris, metas i grups amb una cadena de text.

---
+++ backend/controllers/searchController.js
@@ -0,0 +1,79 @@
+const prisma = require("../config/prisma");
+const utils = require("../helpers/Utils");
+
+const search = async (req, res, next) => {
+  try {
+    const searchWord = req.body.search;
+
+    const users = await prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: searchWord,
+            },
+          },
+          {
+            username: {
+              contains: searchWord,
+            },
+          },
+        ],
+      },
+    });
+
+    const metas = await prisma.meta.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: searchWord,
+            },
+          },
+          {
+            description: {
+              contains: searchWord,
+            },
+          },
+        ],
+      },
+      include: {
+        category: true,
+        author: true,
+      },
+    });
+
+    const groups = await prisma.group.findMany({
+      where: {
+        OR: [
+          {
+            name: searchWord,
+          },
+          {
+            description: searchWord,
+          },
+        ],
+      },
+      include: {
+        owner: true,
+        groupUsers: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+        users,
+        metas: utils.handleBigInt(metas),
+        groups
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+    search
+};
