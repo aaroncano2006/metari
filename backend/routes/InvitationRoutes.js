a6d14643648e69f2701ecdbba176a9d2cbcca5a9
Aaron Cano <aaroncanofdez@gmail.com>
2026-05-17 21:32:32 +0200

    Rutes d'invitacions protegides.

--- backend/routes/InvitationRoutes.js
+++ backend/routes/InvitationRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const invitationController = require('../controllers/InvitationController');
+const { isAuthenticated, isAdmin } = require('../middlewares/auth/authorize');
 
-router.get('/:userid/:sentorreceived/:status', invitationController.getInvitations); // segon param "sent" o "received"
-router.post('/:senderid/:receiverid', invitationController.sendInvitations);
-router.post('/:senderid/:receiverid/:groupid', invitationController.sendInvitations);
-router.put('/:receiverid/:id', invitationController.acceptInvitation); // id: id de la invitació
-router.delete('/:userid/:id', invitationController.rejectInvitation);
+router.get('/:userid/:sentorreceived/:status', isAuthenticated, invitationController.getInvitations); // segon param "sent" o "received"
+router.post('/:senderid/:receiverid', isAuthenticated, invitationController.sendInvitations);
+router.post('/:senderid/:receiverid/:groupid', isAuthenticated, invitationController.sendInvitations);
+router.put('/:receiverid/:id', isAuthenticated, invitationController.acceptInvitation); // id: id de la invitació
+router.delete('/:userid/:id', isAuthenticated, invitationController.rejectInvitation);
 
-router.get('/friends/:userid', invitationController.getFriendsByID);
+router.get('/friends/:userid', isAuthenticated, invitationController.getFriendsByID);
 
 // userid pot ser tant la id de l'emissor com la del
 // receptor
