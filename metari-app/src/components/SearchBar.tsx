41d03691ba01ede8758f1205304e96d966716d41
Aaron Cano <aaroncanofdez@gmail.com>
2026-05-17 17:12:53 +0200

    Ús de searchSchema per evitar cerques buides per part dels usuaris.

--- metari-app/src/components/SearchBar.tsx
+++ metari-app/src/components/SearchBar.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { searchSchema } from "../schemas/searchSchema";
 
 export default function SearchBar() {
+  const [errors, setErrors] = useState<Record<string, string>>({});
   const [searchParams] = useSearchParams();
   const word = searchParams.get("q") ?? "";
   const [formData, setFormData] = useState<any>({
@@ -12,6 +14,19 @@ export default function SearchBar() {
   const navigate = useNavigate();
 
   const submitSearch = (data: any) => {
+    const validation = searchSchema.safeParse(data);
+    if (!validation.success) {
+      const errors: Record<string, string> = {};
+
+      validation.error.issues.forEach((issue) => {
+        const field = issue.path[0] as string;
+        errors[field] = issue.message;
+      });
+
+      return setErrors(errors);
+    }
+    setErrors({});
+
     navigate(`/search?q=${data.word}`);
   };
 
@@ -37,6 +52,9 @@ export default function SearchBar() {
             placeholder="Cerca"
           />
         </div>
+        {errors.word && (
+            <small className="text-danger d-flex mb-2 mt-1">{errors.word}</small>
+          )}
       </form>
     </>
   );
