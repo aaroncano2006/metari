5da9a0c3da499912eaa0213ae4bbe6cf6a7a823e
Aaron Cano <aaroncanofdez@gmail.com>
2026-05-18 12:18:46 +0200

    A l'igual que en Users, ara s'utilitza req.user.id extret del token per evitar que es manipulin invitacions on l'usuari no està involucrat.

--- backend/routes/InvitationRoutes.js
+++ backend/routes/InvitationRoutes.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const invitationController = require('../controllers/InvitationController');
-const { isAuthenticated, isAdmin } = require('../middlewares/auth/authorize');
+const { isAuthenticated } = require('../middlewares/auth/authorize');
 
-router.get('/:userid/:sentorreceived/:status', isAuthenticated, invitationController.getInvitations); // segon param "sent" o "received"
+router.get('/:userid/:sentorreceived/:status', isAuthenticated, invitationController.getInvitations);
 router.post('/:senderid/:receiverid', isAuthenticated, invitationController.sendInvitations);
 router.post('/:senderid/:receiverid/:groupid', isAuthenticated, invitationController.sendInvitations);
-router.put('/:receiverid/:id', isAuthenticated, invitationController.acceptInvitation); // id: id de la invitació
-router.delete('/:userid/:id', isAuthenticated, invitationController.rejectInvitation);
+router.put('/:receiverid/:id', isAuthenticated, invitationController.acceptInvitation);
+router.delete('/:id', isAuthenticated, invitationController.rejectInvitation);
 
 router.get('/friends/:userid', isAuthenticated, invitationController.getFriendsByID);
-
-// userid pot ser tant la id de l'emissor com la del
-// receptor
-
-// Les rutes hauran de ser refactoritzades quan s'implementi el login amb JWT.
 
 module.exports = router;
