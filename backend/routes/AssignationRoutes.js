7e298f0fe3028fbf3963418a787db271e10c59a1
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-20 19:49:52 +0200

    De moment no s'utilitza isAdmin en assignacios. A valorar els diferents casos d'ús.

--- backend/routes/AssignationRoutes.js
+++ backend/routes/AssignationRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const assignationController = require('../controllers/AssignationController');
-const { isAuthenticated, isAdmin } = require('../middlewares/auth/authorize');
+const { isAuthenticated } = require('../middlewares/auth/authorize');
 
 // get totes les assignacions
 router.get('/', assignationController.getAssignations);
 router.get('/:id', assignationController.getAssignationById);
 router.post('/', isAuthenticated, assignationController.createAssignation);
-router.delete('/:id', isAuthenticated, isAdmin, assignationController.deleteAssignation);
-router.put('/:id', isAuthenticated, isAdmin, assignationController.updateAssignation);
+router.delete('/:id', isAuthenticated, assignationController.deleteAssignation);
+router.put('/:id', isAuthenticated, assignationController.updateAssignation);
 
 module.exports = router;
 
