bf979e43bf4c47df17b72dda3c189eb49ed0356b
acanodev <aaroncanofdez@gmail.com>
2026-05-20 23:32:24 +0200

    GET d'usuaris protegits per evitar interaccions amb la comunitat si no está registrat en l'aplicació (limitacions de GUEST)

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/UserController');
 const { isAuthenticated, isAdmin } = require('../middlewares/auth/authorize');
 
-router.get('/', userController.getUsuaris);
-router.get('/:id', userController.getUsuariById);
-router.post('/', userController.createUsuari);
+router.get('/', isAuthenticated, userController.getUsuaris);
+router.get('/:id', isAuthenticated, userController.getUsuariById);
+router.post('/', userController.createUsuari); // Ruta pública per registrar-se.
 router.put('/:id', isAuthenticated, userController.updateUsuari);
 router.delete('/:id', isAuthenticated, isAdmin, userController.deleteUsuari);
 
