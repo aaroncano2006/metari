1b932ab82b70eddb1bd4dbaee73787ffed1c38a6
acanodev <aaroncanofdez@gmail.com>
2026-05-20 23:24:27 +0200

    Interceptor a axiosConnection per agafar el token i enviar-ho al header Authorization.

--- metari-app/src/services/axiosConnection.ts
+++ metari-app/src/services/axiosConnection.ts
@@ -2,7 +2,26 @@ import axios from "axios"
 
 export const axiosConnection = axios.create({
   baseURL: "http://localhost:3001/api",
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
 })
+
+axiosConnection.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+axiosConnection.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token")
+      window.dispatchEvent(new CustomEvent("authChange"))
+    }
+    return Promise.reject(error)
+  }
+)
