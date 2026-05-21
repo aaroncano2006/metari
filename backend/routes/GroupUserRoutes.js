48c61a761ce84d1c4b4e126757f2507f62c12201
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-21 16:04:50 +0200

    Rutes GroupUser protegides.

--- backend/routes/GroupUserRoutes.js
+++ backend/routes/GroupUserRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const groupUserController = require('../controllers/GroupUserController');
+const { isAuthenticated } = require('../middlewares/auth/authorize');
 
 router.get('/', groupUserController.getGroupUsers);
 router.get('/:group_id/:user_id', groupUserController.getGroupUser);
-router.post('/', groupUserController.createGroupUser);
-router.put('/:group_id/:user_id', groupUserController.updateGroupUser);
-router.delete('/:group_id/:user_id', groupUserController.deleteGroupUser);
+router.post('/', isAuthenticated, groupUserController.createGroupUser);
+router.put('/:group_id/:user_id', isAuthenticated, groupUserController.updateGroupUser);
+router.delete('/:group_id/:user_id', isAuthenticated, groupUserController.deleteGroupUser);
 
 module.exports = router;
