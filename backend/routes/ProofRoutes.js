4c52cae8a9194f8f762e6743c7399fbc20309b35
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-05-21 15:55:33 +0200

    Linies comentades eliminades.

--- backend/routes/ProofRoutes.js
+++ backend/routes/ProofRoutes.js
@@ -25,13 +25,4 @@ router.put("/:id", isAuthenticated, (req, res, next) => {
 
 router.delete('/:id', isAuthenticated, isAdmin, proofController.deleteProof);
 
-/*const { isAuthenticated, isAdmin } = require('../middlewares/auth/authorize');
-
-router.get('/', proofController.getProofs);
-router.get('/:id', proofController.getProofById);
-router.post('/', isAuthenticated, proofController.createProof);
-router.put('/:id', isAuthenticated, proofController.updateProof);
-router.delete('/:id', isAuthenticated, isAdmin, proofController.deleteProof);*/
-
-
 module.exports = router;
